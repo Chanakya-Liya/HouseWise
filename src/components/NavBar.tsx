9396dd16f19a52ebea0add6ccc6ebffd80c01b51
unknown <cliyanage124@gmail.com>
2025-01-06 03:54:34 +0530

    nav bar css

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,35 +1,47 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { NavLink } from 'react-router-dom';
 import styles from './Navbar.module.css';
 
 const Navbar: React.FC = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuOpen(prevState => !prevState);
+  };
+
   return (
-    <nav className={styles.navbar}>
+    <nav className={`${styles.navbar} ${menuOpen ? styles.open : ''}`}>
       <div className={styles.logo}>
         <a href="/">HouseWise</a>
       </div>
-      {/* Navigation links */}
-      <div className={styles.links}>
-        <NavLink 
-          to="/" 
+
+      {/* Hamburger Icon */}
+      <div className={styles.hamburger} onClick={toggleMenu}>
+        &#9776;
+      </div>
+
+      {/* Navigation Links */}
+      <div className={`${styles.links} ${menuOpen ? styles.active : ''}`}>
+        <NavLink
+          to="/"
           className={({ isActive }) => isActive ? styles.active : ''}
         >
           Home
         </NavLink>
-        <NavLink 
-          to="/view-all-properties" 
+        <NavLink
+          to="/view-all-properties"
           className={({ isActive }) => isActive ? styles.active : ''}
         >
           View All
         </NavLink>
-        <NavLink 
-          to="/favourites" 
+        <NavLink
+          to="/favourites"
           className={({ isActive }) => isActive ? styles.active : ''}
         >
           Favourites
         </NavLink>
-        <NavLink 
-          to="/contact-us" 
+        <NavLink
+          to="/contact-us"
           className={({ isActive }) => isActive ? styles.active : ''}
         >
           Contact Us
